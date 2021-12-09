7e02c08b17eb5d8d10c8e7070d804301a8eda74d
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-09 07:45:05 -0300

    initial commit.

---
+++ brewey-client/src/main/java/artursfg/breweyclient/BreweyClientApplication.java
@@ -0,0 +1,13 @@
+package artursfg.breweyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweyClientApplication.class, args);
+	}
+
+}
---
+++ brewey-client/src/test/java/artursfg/breweyclient/BreweyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package artursfg.breweyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
